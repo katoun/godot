524cc818006776bbd299be60104c505120188844
Catalin Alexandru Nastase <katoun9@gmail.com>
2026-08-04 18:42:24 +0300

    add world2d res extention

--- scene/resources/world_2d.h
+++ scene/resources/world_2d.h
@@ -42,7 +42,7 @@ struct SpatialIndexer2D;
 
 class World2D : public Resource {
 	GDCLASS(World2D, Resource);
-	RES_BASE_EXTENSION("world");
+	RES_BASE_EXTENSION("world2d");
 
 	RID canvas;
 #ifndef NAVIGATION_2D_DISABLED
